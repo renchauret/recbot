7d2540462f881b1458f86ba8cc69135621713ea9
Ren Chauret <renaudchauret@gmail.com>
2025-09-25 15:21:12 -0400

    recd

--- commands/recd.ts
+++ commands/recd.ts
@@ -1,12 +1,12 @@
 import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js'
 import type { RecbotCommand } from './commands.ts'
 import type { Profile } from '../models/profile.ts'
-import { getOrCreateProfile } from '../db/utils.ts'
+import { getOrCreateProfile, saveProfile } from '../db/utils.ts'
 
 export const recd: RecbotCommand = {
     data: new SlashCommandBuilder()
         .setName('recd')
-        .setDescription('Shows you your rec queue.')
+        .setDescription('Deletes one item from your rec queue.')
         .addNumberOption(option =>
             option.setName('index')
                 .setDescription('Enter the index of the rec to delete (first is 0).')
@@ -15,9 +15,11 @@ export const recd: RecbotCommand = {
     execute: async (interaction: ChatInputCommandInteraction) => {
         const user = interaction.user
         const indexToDelete = interaction.options.getNumber('index')
+        console.log(indexToDelete)
         const profile: Profile = getOrCreateProfile(interaction.guildId, user.id, user.displayName)
         const recToDelete = profile.recs[indexToDelete]
-        profile.recs = profile.recs.splice(indexToDelete, 1)
+        profile.recs.splice(indexToDelete, 1)
+        saveProfile(interaction.guildId, profile)
         await interaction.reply(`${user.displayName} deleted rec ${recToDelete}`)
     }
 }
