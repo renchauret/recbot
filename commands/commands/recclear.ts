ebd0122fcd7dbaacf5cfc3d4cc044f84f3112831
Ren Chauret <renaudchauret@gmail.com>
2025-09-26 12:14:56 -0400

    migrate recclear

--- commands/commands/recclear.ts
+++ commands/commands/recclear.ts
@@ -1,7 +1,7 @@
 import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js'
 import type { RecbotCommand } from '../commands.ts'
 import type { Profile } from '../../models/profile.ts'
-import { getOrCreateProfile, saveProfile } from '../../db/utils.ts'
+import { createProfileOrUpdateDisplayName, saveRecsToProfile } from '../../db/db.ts'
 
 export const recclear: RecbotCommand = {
     data: new SlashCommandBuilder()
@@ -9,9 +9,9 @@ export const recclear: RecbotCommand = {
         .setDescription('Deletes all items from your rec queue.'),
     execute: async (interaction: ChatInputCommandInteraction) => {
         const user = interaction.user
-        const profile: Profile = getOrCreateProfile(interaction.guildId, user.id, user.displayName)
+        const profile: Profile = await createProfileOrUpdateDisplayName(interaction.guildId, user.id, user.displayName)
         profile.recs = []
-        saveProfile(interaction.guildId, profile)
+        await saveRecsToProfile(interaction.guildId, profile.id, profile.recs)
         await interaction.reply(`${user.displayName} deleted all recs from their queue`)
     }
 }
