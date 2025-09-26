c3f38c7d60127f328df548eb5ac3084f4e69e972
Ren Chauret <renaudchauret@gmail.com>
2025-09-26 12:16:20 -0400

    migrate recq

--- commands/commands/recq.ts
+++ commands/commands/recq.ts
@@ -1,7 +1,7 @@
 import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js'
 import type { RecbotCommand } from '../commands.ts'
 import type { Profile } from '../../models/profile.ts'
-import { getOrCreateProfile } from '../../db/utils.ts'
+import { createProfileOrUpdateDisplayName } from '../../db/db.ts'
 
 export const recq: RecbotCommand = {
     data: new SlashCommandBuilder()
@@ -9,7 +9,7 @@ export const recq: RecbotCommand = {
         .setDescription('Shows you your rec queue.'),
     execute: async (interaction: ChatInputCommandInteraction) => {
         const user = interaction.user
-        const profile: Profile = getOrCreateProfile(interaction.guildId, user.id, user.displayName)
+        const profile: Profile = await createProfileOrUpdateDisplayName(interaction.guildId, user.id, user.displayName)
         const formattedRecs = profile.recs.map(((rec, index) => `${index}: <${rec}>`)).join('\n')
         await interaction.reply(`${user.displayName}'s rec queue:\n${formattedRecs}`)
     }
