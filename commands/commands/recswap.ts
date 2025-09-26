d06bae19f0847c51852b476497c5ab9b7a354b6a
Ren Chauret <renaudchauret@gmail.com>
2025-09-26 12:16:56 -0400

    migrate recswap

--- commands/commands/recswap.ts
+++ commands/commands/recswap.ts
@@ -1,7 +1,7 @@
 import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js'
 import type { RecbotCommand } from '../commands.ts'
 import type { Profile } from '../../models/profile.ts'
-import { getOrCreateProfile, saveProfile } from '../../db/utils.ts'
+import { createProfileOrUpdateDisplayName, saveRecsToProfile } from '../../db/db.ts'
 
 export const recswap: RecbotCommand = {
     data: new SlashCommandBuilder()
@@ -21,12 +21,12 @@ export const recswap: RecbotCommand = {
         const user = interaction.user
         const index1 = interaction.options.getNumber('index1')
         const index2 = interaction.options.getNumber('index2')
-        const profile: Profile = getOrCreateProfile(interaction.guildId, user.id, user.displayName)
+        const profile: Profile = await createProfileOrUpdateDisplayName(interaction.guildId, user.id, user.displayName)
         const rec1ToSwap = profile.recs[index1]
         const rec2ToSwap = profile.recs[index2]
         profile.recs[index1] = rec2ToSwap
         profile.recs[index2] = rec1ToSwap
-        saveProfile(interaction.guildId, profile)
+        await saveRecsToProfile(interaction.guildId, profile.id, profile.recs)
         await interaction.reply(`${user.displayName} swapped recs <${rec1ToSwap}> and <${rec2ToSwap}>`)
     }
 }
