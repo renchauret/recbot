5ddc91db58120fd3d69fb298259a3d840a0df4bb
Ren Chauret <renaudchauret@gmail.com>
2025-09-26 16:21:38 -0400

    random message

--- cron/prompt-discussion.ts
+++ cron/prompt-discussion.ts
@@ -2,6 +2,7 @@ import { CronJob } from 'cron'
 import { getConfig } from '../config/config.ts'
 import { getChannel } from '../discord/discord-client.ts'
 import { getAllGuildIds, getOrCreateGuild, getMostRecentPickedRec } from '../db/db.ts'
+import { randomInt } from 'crypto'
 
 const promptDiscussion = async (guildId: string) => {
     const preferredChannelId = (await getOrCreateGuild(guildId))?.preferredChannelId
@@ -21,8 +22,7 @@ const promptDiscussion = async (guildId: string) => {
         return
     }
 
-    // todo random message
-    await channel.send(`How was ${latestPickedRec.name}?`)
+    await channel.send(messageOptions[randomInt(0, messageOptions.length)](latestPickedRec.name))
 }
 
 const promptDiscussions = async () => {
@@ -40,6 +40,16 @@ export const startPromptDiscussionJob = () => {
     console.log(`started promptdiscussion job with cronTime ${cron}`)
 }
 
-const messageOptions = [
-
+const messageOptions: ((recName: string) => string)[] = [
+    (recName: string) => `Did you like ${recName}?`,
+    (recName: string) => `What was your favorite track on ${recName}?`,
+    (recName: string) => `What was your least favorite track on ${recName}?`,
+    (recName: string) => `What's one thing that ${recName} did well?`,
+    (recName: string) => `What's one thing that ${recName} did not do well?`,
+    (recName: string) => `What would you rate ${recName} out of 10?`,
+    (recName: string) => `What were you doing while listening to ${recName}? Did it suit that activity well?`,
+    (recName: string) => `What did ${recName} make you think of?`,
+    (recName: string) => `What emotion did you feel while listening to ${recName}?`,
+    (recName: string) => `Did you add ${recName} to your own library?`,
+    (recName: string) => `What's worth saying about ${recName}?`,
 ]
