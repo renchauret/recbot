520f116089ca0c7a75c20318930d9ee4dabf982f
Ren Chauret <renaudchauret@gmail.com>
2025-09-26 17:12:31 -0400

    again

--- config/config.ts
+++ config/config.ts
@@ -4,7 +4,7 @@ export type Config = {
 }
 
 const prodConfig: Config = {
-    pickRecCron: '0 13 17 * * 5',
+    pickRecCron: '0 15 17 * * 5',
     promptDiscussionCron: '0 0 16 * * 5'
 }
 const devConfig: Config = {
