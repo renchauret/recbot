63a7ec1f11ac3b6c33953d1cbfdb941609427540
Ren Chauret <renaudchauret@gmail.com>
2025-09-26 20:06:13 -0400

    fix text

--- cron/prompt-discussion.ts
+++ cron/prompt-discussion.ts
@@ -45,15 +45,15 @@ export const startPromptDiscussionJob = () => {
 }
 
 const messageOptions: ((recName: string) => string)[] = [
-    (recName: string) => `Did you like ${recName}?`,
-    (recName: string) => `What was your favorite track on ${recName}?`,
-    (recName: string) => `What was your least favorite track on ${recName}?`,
+    (recName: string) => `Did you like ${recName} ?`,
+    (recName: string) => `What was your favorite track on ${recName} ?`,
+    (recName: string) => `What was your least favorite track on ${recName} ?`,
     (recName: string) => `What's one thing that ${recName} did well?`,
     (recName: string) => `What's one thing that ${recName} did not do well?`,
     (recName: string) => `What would you rate ${recName} out of 10?`,
     (recName: string) => `What were you doing while listening to ${recName}? Did it suit that activity well?`,
     (recName: string) => `What did ${recName} make you think of?`,
-    (recName: string) => `What emotion did you feel while listening to ${recName}?`,
+    (recName: string) => `What emotion did you feel while listening to ${recName} ?`,
     (recName: string) => `Did you add ${recName} to your own library?`,
-    (recName: string) => `What's worth saying about ${recName}?`,
+    (recName: string) => `What's worth saying about ${recName} ?`,
 ]
